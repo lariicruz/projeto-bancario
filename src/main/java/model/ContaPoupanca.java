a102d250d6129d0dcbc0b30a5726f7af28b496a7
Larissa Cruz <mlp-larissa@hotmail.com>
2021-11-12 15:18:54 -0300

    Ajustes na Conta poupança

--- src/main/java/model/ContaPoupanca.java
+++ src/main/java/model/ContaPoupanca.java
@@ -16,9 +16,7 @@ public class ContaPoupanca extends Conta {
     @Override
     public void imprimirExtrato() {
         System.out.println("** Extrato Conta Poupança ***");
-        System.out.println(String.format("Agencia: %d", super.agencia));
-        System.out.println(String.format("Numero: %d", super.numero));
-        System.out.println(String.format("Saldo: %.2f", super.saldo));
+        imprimirInfoComuns();
     }
 }
 
